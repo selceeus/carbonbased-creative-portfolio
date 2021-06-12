487289db3f97de5b83fc6545b5b21128f2f54325
selceeus <matt@carbonbasedcreative.com>
2021-06-12 12:12:31 -0500

    Small Changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@
 import './App.scss';
 
 function App() {
-  return (
-    <section className="App">
-        <header className="App-header" role="region">
-            <img src="logo512.png" className="App-logo" alt="logo" />
-        </header>
-        <main role="main">
-            <p>Hello to new site - This is a test again</p>
-        </main>
-        <footer role="region">
-            <p>this is the footer</p>
-        </footer>
-    </section>
-  );
+    return (
+        <section className="App">
+            <header className="App-header" role="region">
+                <img src="logo512.png" className="App-logo" alt="logo" />
+            </header>
+            <main role="main">
+                <p>Hello to new site - This is a test again</p>
+            </main>
+            <footer role="region">
+                <p>this is the footer</p>
+            </footer>
+        </section>
+    );
 }
 
 export default App;
