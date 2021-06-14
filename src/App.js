237ee8bf8cd5e994d9844cb807e203e07dc039a8
selceeus <matt@carbonbasedcreative.com>
2021-06-13 21:27:26 -0500

    Close down for the night

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ function App() {
             <header className="App-header" role="region">
                 <img src="logo512.png" className="App-logo" alt="logo" />
                 <Route render={({location}) => (
-                  <TransitionGroup>
-                  <CSSTransition key={location.key} classNames="fade" timeout={300}>
-                      <Switch>
-                          <Route exact path='/' component={Homepage} />
-                          <Route path='/city-listing/:cityName' component={CityListing}  />
-                          <Route path='/map' component={Map} />
-                          <Route exact path='/detailed-listing/:breweryId' component={DetailedListing} />
-                          <Route path='/contact' component={Contact} />
-                          <Route path='/sign-up' component={Signup} />
-                          <Redirect to='/' />
-                      </Switch>
-                    </CSSTransition>
-                  </TransitionGroup>
+                    <TransitionGroup>
+                        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                            <Switch>
+                                {/*<Route exact path='/' component={Homepage} />*/}
+                                {/*<Route path='/city-listing/:cityName' component={CityListing} />*/}
+                                {/*<Route path='/map' component={Map} />*/}
+                                {/*<Route exact path='/detailed-listing/:breweryId' component={DetailedListing} />*/}
+                                {/*<Route path='/contact' component={Contact} />*/}
+                                {/*<Route path='/sign-up' component={Signup} />*/}
+                                {/*<Redirect to='/' />*/}
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
                 )} />
             </header>
             <main role="main">
