e076deca066a4e98f50f028c39f7afd016b2cf0d
selceeus <matt@carbonbasedcreative.com>
2021-06-13 21:01:32 -0500

    Added Router DOM - Transition group

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 //import logo from '/logo.svg';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
 
 function App() {
@@ -6,6 +8,21 @@ function App() {
         <section className="App">
             <header className="App-header" role="region">
                 <img src="logo512.png" className="App-logo" alt="logo" />
+                <Route render={({location}) => (
+                  <TransitionGroup>
+                  <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                      <Switch>
+                          <Route exact path='/' component={Homepage} />
+                          <Route path='/city-listing/:cityName' component={CityListing}  />
+                          <Route path='/map' component={Map} />
+                          <Route exact path='/detailed-listing/:breweryId' component={DetailedListing} />
+                          <Route path='/contact' component={Contact} />
+                          <Route path='/sign-up' component={Signup} />
+                          <Redirect to='/' />
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                )} />
             </header>
             <main role="main">
                 <p>Hello to new site - This is a test again</p>
