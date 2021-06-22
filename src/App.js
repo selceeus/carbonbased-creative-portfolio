72a0d10cb5996df7d1fa2496b1f92a830251a42e
selceeus <matt@carbonbasedcreative.com>
2021-06-22 17:28:52 -0500

    Adding Axios, JWT Auth, with Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 //import logo from '/logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
 
+
 function App() {
+
+    const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+        const secretToken = "ry5N+C*(||&B@_-Z~I|_|@D[;?-)!mfh#AX(;$~+Agm";
+
+        const api = "https://cms.matthewa.development/wp-json/wp/v2/pages";
+        
+        const result = await axios
+        .get(api, { headers: {"Authorization" : `Bearer ${token}`} } )
+        .then(result => setData(result.data))
+
+      };
+   
+      fetchData();
+      
+    }, []);
+
     return (
         <section className="App">
             <header className="App-header" role="region">
                 <img src="logo512.png" className="App-logo" alt="logo" />
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>
+                            <h3 className="titles">{item.slug}</h3>
+                        </li>
+                    ))}
+                </ul>
                 {/*<Route render={({location}) => (
                     <TransitionGroup>
                         <CSSTransition key={location.key} classNames="fade" timeout={300}>
