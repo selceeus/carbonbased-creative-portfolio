125bb5461ee70f9c077697593fc39c2ce5428c23
selceeus <matt@carbonbasedcreative.com>
2021-07-09 11:16:58 -0600

    Continued restructure to page components management

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './contact.styles.scss';
 
+const {REACT_APP_API_URL} = process.env;
+
+const authAxios = axios
+    .create({
+        baseUrl: REACT_APP_API_URL,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+});
+
 const Contact = props => {
+
+    const [data, setData] = useState([]);
+    const [requestError, setRequestError] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await authAxios
+                    .get( `${REACT_APP_API_URL}wp-json/wp/v2/pages/14` )
+                    .then( result => setData(result.data) );
+
+            } catch(err) {
+                setRequestError(err.message);
+            }
+            
+        };
+        
+        fetchData();
+    }, []);
+
     return(
         <section className="contact">
             <h1>Contact</h1>
+            {console.log(data)}
         </section>
     );
 }
--- src/pages/solutions/solutions.component.jsx
+++ src/pages/solutions/solutions.component.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './solutions.styles.scss';
 
+const {REACT_APP_API_URL} = process.env;
+
+const authAxios = axios
+    .create({
+        baseUrl: REACT_APP_API_URL,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+});
+
 function Solutions() {
+
+    const [data, setData] = useState([]);
+    const [requestError, setRequestError] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await authAxios
+                    .get( `${REACT_APP_API_URL}wp-json/wp/v2/pages/64` )
+                    .then( result => setData(result.data) );
+
+            } catch(err) {
+                setRequestError(err.message);
+            }
+            
+        };
+        
+        fetchData();
+    }, []);
+
     return(
         <section className="solutions">
             <h1>Solutions</h1>
+            {console.log(data)}
         </section>
     );
 }
