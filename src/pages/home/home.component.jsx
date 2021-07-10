6db8c62ed802cc353f424317022f2a24c8560c7f
selceeus <matt@carbonbasedcreative.com>
2021-07-10 10:47:44 -0600

    Created render function for Home Component

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import parse from 'html-react-parser';
+import parse from 'html-react-parser';
 import './home.styles.scss';
 
 const {REACT_APP_API_URL} = process.env;
@@ -23,21 +23,36 @@ function Home() {
             try {
                 const result = await authAxios
                     .get( `${REACT_APP_API_URL}wp-json/wp/v2/pages/62` )
-                    .then( result => setData(result.data) );
+                    .then( result => setData({content: result.data}) );
 
             } catch(err) {
                 setRequestError(err.message);
             }
-            
         };
         
         fetchData();
     }, []);
 
+    const renderPage = apiData => {
+
+        if(!Object.keys(apiData).length > 0) {
+            return "<Loader />";
+        }
+        else {
+            return (
+                <div>
+                    <h1>{apiData.title.rendered}</h1>
+                    <p>{parse(apiData.content.rendered)}</p>
+                    <p>{parse(apiData.acf.site_lead.content)}</p>
+                </div>
+            );
+        }
+
+    }
+
     return(
         <section className="home">
-            <h1>Home</h1>
-            {console.log(data)}
+            {renderPage(data.content)}
         </section>
     );
 }
