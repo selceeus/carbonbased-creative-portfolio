185c9d1f0d3903cfa0c7c4ff88adf3055e391ee5
selceeus <matt@carbonbasedcreative.com>
2021-07-14 19:10:12 -0600

    Added framer motion to footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import './footer.styles.scss';
 
 const Footer = props => {
     return(
-        <motion.div
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1, y: 100  }}
-            exit={{ opacity: 0 }}
-        >
-            <footer>
-                <p>this is the footer</p>
-            </footer>
-        </motion.div>
+        <footer>
+            <p>this is the footer</p>
+        </footer>
     );
 }
 
