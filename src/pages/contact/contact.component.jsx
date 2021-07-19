8572e793c3a585f1d2a77360491b4ba01c74c1f7
selceeus <matt@carbonbasedcreative.com>
2021-07-18 21:23:45 -0600

    Started wiring up the contact component with controlled form and map

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import dompurify from 'dompurify';
+import { motion } from 'framer-motion';
+
+//Imported Components 
+import Loader from '../../components/utilities/loader.component';
 import './contact.styles.scss';
 
 const {REACT_APP_API_URL} = process.env;
@@ -12,10 +17,69 @@ const authAxios = axios
         }
 });
 
-const Contact = props => {
+function Contact() {
 
     const [data, setData] = useState([]);
     const [requestError, setRequestError] = useState([]);
+    const sanitize = dompurify.sanitize;
+
+    const renderContactHero = apiData => {
+
+        const siteLead = () => { return{ __html: sanitize(apiData.acf.site_lead.content) } };
+
+        return (
+            <div className="hero">
+            {apiData.acf.site_lead.content && <div dangerouslySetInnerHTML={siteLead()}></div>}
+            </div>
+        );
+    }
+
+    const renderContactForm = apiData => {
+
+        const siteLead = () => { return{ __html: sanitize(apiData.acf.site_lead.content) } };
+
+        return (
+            <div className="hero">
+            {apiData.acf.site_lead.content && <div dangerouslySetInnerHTML={siteLead()}></div>}
+            </div>
+        );
+    }
+
+    const renderContactInformation = apiData => {
+
+        const siteLead = () => { return{ __html: sanitize(apiData.acf.site_lead.content) } };
+
+        return (
+            <div className="hero">
+            {apiData.acf.site_lead.content && <div dangerouslySetInnerHTML={siteLead()}></div>}
+            </div>
+        );
+    }
+
+    const renderContactMap = apiData => {
+
+        const siteLead = () => { return{ __html: sanitize(apiData.acf.site_lead.content) } };
+
+        return (
+            <div className="hero">
+            {apiData.acf.site_lead.content && <div dangerouslySetInnerHTML={siteLead()}></div>}
+            </div>
+        );
+    }
+
+    const renderPage = apiData => {
+
+        if(!Object.keys(apiData).length > 0) {
+            return <Loader />;
+        }
+        else {
+            return(
+                <React.Fragment>
+                    {renderContactHero(apiData)}
+                </React.Fragment>
+            )
+        }
+    }
 
     useEffect(() => {
         const fetchData = async () => {
